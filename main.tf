46d2e86b8e48a6b9f303edabf86a9e94aec60959
therandomaccesss-max <therandomaccesss@gmail.com>
2026-02-24 20:59:43 +0530

    Added a varable.tf file and added a s3 bucket in config file

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "aws_instance" "demo-instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
 }
+
+resource "aws_s3_bucket" "demo-bucket" {
+  bucket = var.bucket_name
+}
+
